e0d90d7bc20a90185a78319940b2127c2766ea95
Maud Galametz <maud.galametz@gmail.com>
2018-12-11 02:14:40 +0100

    Add files via upload

---
+++ CarbonFootprint.py
@@ -0,0 +1,34 @@
+def CarbonFootprint(file):
+
+	from geopy.geocoders import Nominatim
+	from geopy.distance import geodesic
+	import pandas
+
+	data = pandas.read_csv(file, names=['Origin','Destination'], delimiter=';',dtype=None)
+
+	#Origin = ["Paris"]
+	#Destination = ["New York"]
+	Total = 0
+
+	for i in range(len(data['Origin'])):
+
+		geolocator = Nominatim(user_agent="specify_your_app_name_here")
+
+		location1 = geolocator.geocode(data['Origin'][i])
+		#print(location1.latitude, location1.longitude)
+
+		location2 = geolocator.geocode(data['Destination'][i])
+		#print(location2.latitude, location2.longitude)
+
+		#print(data['Origin'][i] + "-->" + data['Destination'] + " in kilometers")
+		dist = geodesic((location1.latitude, location1.longitude),(location2.latitude, location2.longitude)).miles/0.62137
+		if dist < 1500:
+			Total = Total + (2*dist)*0.15 #short-flights - in Co2 kg /eq_passeger/km 
+		else: 
+			Total = Total + (2*dist)*0.12 #long-flights  - in Co2 kg /eq_passeger/km
+		
+	print('Total Carbon footprint in tons CO2:')
+	print(Total/1000.)
+	print('Total Carbon footprint in tons CO2 including radiative forcing:')
+	print(Total/1000.)*1.891 	# (as recommended by the Department for Environment, Food & Rural Affairs)
+
---
+++ README.md
@@ -0,0 +1,40 @@
+# Calculate your carbon footprint
+
+
+### Description
+
+Calculate your carbon footprint from a csv file containing your travels with Origin and Destination as two separate columns. 
+
+The script calculates the geodesic distance return in km and multiplies by an average value of the CO2 equivalent passenger / km factor
+depending on the distance of the flight. The average has been estimated using the values provided by the French DGAC estimator:
+\url{https://eco-calculateur.dta.aviation-civile.gouv.fr/}
+
+To take radiative forcing into account, we multiply the emission by 1.891 as recommended by the UK Department for Environment, Food \& Rural Affairs
+
+
+
+## How to use the script Carbonfootprint.py:
+
+**Inputs:** 
+
+file: csv file containing the Origin and Destination as two separate columns
+
+**Output** 
+
+The total CO2 emission in tons including or nor the radiative forcing.
+
+
+**Example** 
+
+Carbonfootprint('ListFlight.csv')
+
+
+## Versioning
+
+We use python 2.7.12. 
+
+
+## Authors
+
+* **Maud Galametz** 
+
